528d38dbdde31ca6b7e810fa4ea20b91ad401590
João Machado <fentanes.machado@gmail.com>
2026-03-31 18:04:46 -0500

    dada

--- word_search.py
+++ word_search.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 
+# A script to find regular expressions within a txt file
 import re
 
 if __name__ == "__main__":
-    herit_pattern_str = r"\w*herit\w*"
+    herit_pattern_str = r"^\w*herit\w*$"
     herit_pattern = re.compile(herit_pattern_str, re.IGNORECASE)
 
 print('Opening origin.txt')
@@ -13,10 +14,10 @@ with open('origin.txt', 'r') as in_stream:
         for line_index, line in enumerate(in_stream):
             line = line.strip()
             word_list = line.split()
-            herit_pattern.findall(word_list)
             for word in word_list:
-                out_stream.write(f'{line_index + 1}\t{word}\n')
+                if herit_pattern.search(word):
+                    out_stream.write(f'{line_index + 1}\t{word}\n')
 
 print("Done!")
 print('origin.txt is closed?', in_stream.closed)
-print('herit_in_origin.txt.txt is closed?', out_stream.closed)
+print('herit_in_origin.txt is closed?', out_stream.closed)
